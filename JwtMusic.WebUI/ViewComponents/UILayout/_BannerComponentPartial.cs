f7c6886b5acf4f3ae749be9089ff0d1b44785ce9
edaagunes <edagunes00@hotmail.com>
2025-04-29 23:59:11 +0300

    Added Banner in default page

--- JwtMusic.WebUI/ViewComponents/UILayout/_BannerComponentPartial.cs
+++ JwtMusic.WebUI/ViewComponents/UILayout/_BannerComponentPartial.cs
@@ -1,12 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using JwtMusic.BusinessLayer.Abstract;
+using JwtMusic.DtoLayer.BannerDtos;
+using Microsoft.AspNetCore.Mvc;
 
 namespace JwtMusic.WebUI.ViewComponents.UILayout
 {
 	public class _BannerComponentPartial:ViewComponent
 	{
+		private readonly IBannerService _bannerService;
+		private readonly IMapper _mapper;
+
+		public _BannerComponentPartial(IBannerService bannerService, IMapper mapper)
+		{
+			_bannerService = bannerService;
+			_mapper = mapper;
+		}
+
 		public IViewComponentResult Invoke()
 		{
-			return View();
+			var values = _bannerService.TGetAll();
+			var result = _mapper.Map<List<ResultBannerDto>>(values);
+			return View(result);
 		}
 	}
 }
