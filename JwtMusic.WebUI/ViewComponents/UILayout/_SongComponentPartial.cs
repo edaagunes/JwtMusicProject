0b8f614465e009e4cd69c5668cab731d4b5fa34d
edaagunes <edagunes00@hotmail.com>
2025-05-06 22:34:18 +0300

    Added Songs in default page

--- JwtMusic.WebUI/ViewComponents/UILayout/_SongComponentPartial.cs
+++ JwtMusic.WebUI/ViewComponents/UILayout/_SongComponentPartial.cs
@@ -1,12 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using JwtMusic.BusinessLayer.Abstract;
+using JwtMusic.DtoLayer.SongDtos;
+using Microsoft.AspNetCore.Mvc;
 
 namespace JwtMusic.WebUI.ViewComponents.UILayout
 {
 	public class _SongComponentPartial:ViewComponent
 	{
+		private readonly ISongService _songService;
+		private readonly IMapper _mapper;
+
+		public _SongComponentPartial(IMapper mapper, ISongService songService)
+		{
+			_mapper = mapper;
+			_songService = songService;
+		}
+
 		public IViewComponentResult Invoke()
 		{
-			return View();
+			var values = _songService.TGetAll();
+			var result = _mapper.Map<List<ResultSongDto>>(values);
+			return View(result);
 		}
 	}
 }
